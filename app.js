dd5d63cbb72077009e87b0bbe4be5ba748f30fb9
hungnd2905 <hungnd2905@gmail.com>
2022-02-07 17:19:51 +0100

    GET, PUT, PATCH, DELETE one article

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+//////////////////////Requests Targetting all Articles ///////////////////////////
+
+
 //chained route handler using express
 app.route("/articles")
 // RESTful: GET all articles
@@ -67,6 +70,69 @@ app.route("/articles")
       res.send(err);
     }
   });
+});
+
+//////////////////////Requests Targetting A Specific Article ///////////////////////////
+
+// Route parameter: Route path: /users/:userId/books/:bookId
+app.route("/articles/:articleTitle")
+
+//RESTful: Get a specific articles
+.get(function(req,res){
+  Article.findOne({title: req.params.articleTitle}, function(err,foundArticle){
+    if(foundArticle){
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found. ")
+    }
+  });
+})
+
+//RESTful: Put(update) a specific articles completely
+.put(function(req,res){
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+//RESTful: Patch(update) a specific articles partly
+.patch(function(req,res){
+  Article.updateOne(
+    {title:req.params.articleTitle},
+    //$set operator replaces the value of a field with the specified value
+    //E.g. : {$set: {content:"newContentTobeUpdated", value:"newValueTobeUpdated"}}
+    //To make it dynamic: $set: req.body
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.")
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+//RESTful: Delete(remove) a specific articles
+.delete(function(req,res){
+  Article.deleteOne(
+    {title:req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("Successfully deleted article.")
+      } else {
+        res.send(err);
+      }
+    }
+  )
 });
 
 //start server at localhost in port 3000
